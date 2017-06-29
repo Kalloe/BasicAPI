3f609cda0407a5e8f57a36315e2c63657478d1f3
Haylee Heilman <hayleeheilman@Haylees-MacBook-Pro.local>
2017-06-28 21:46:21 -0600

    new stuff

---
+++ BasicAPI/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAPI.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new{id = 1, Name = "New Yourk City"},
+                new{id = 2, Name = "Antwerp"}
+            });
+        }
+    }
+}
