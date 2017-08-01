84a1337af61c74fa4ca6bf78e7301316a390f1af
Haylee Heilman <hayleeheilman@Haylees-MacBook-Pro.local>
2017-08-01 08:17:13 -0600

    new entities

---
+++ BasicAPI/Entities/PointOfInterest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BasicAPI.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+        public int CityId { get; set; }
+    }
+}
